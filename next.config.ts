28d418a7788f245edad224a538299d7bf58f5dc1
Oluwatomisin John Badmus <155178644+tomisin05@users.noreply.github.com>
2025-05-26 22:14:30 -0400

    remove basePath

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: "export",
     reactStrictMode: true,
+    // basePath: '/anant-github-page-test',
+
     images: {
         unoptimized: true,
     },
