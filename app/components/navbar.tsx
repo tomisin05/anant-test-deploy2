3cdaeadeb3801672f6265918c0beead9cbcc46af
Oluwatomisin John Badmus <Tomisinbadmus@gmail.com>
2025-06-18 08:24:16 -0400

    changed logo to png

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import logo from "../images/logo/logo.svg";
+import logo from "../images/logo/logo.png";
 
 
 interface NavItems {
