060c07b2a5e7080552e8c1ea1b50bde21030ec44
Oluwatomisin John Badmus <Tomisinbadmus@gmail.com>
2025-06-19 17:55:13 -0400

    Update About.tsx

--- app/ui/home/About.tsx
+++ app/ui/home/About.tsx
@@ -9,8 +9,8 @@ export default function About() {
     <section id="about" className="relative py-28 px-4 md:px-12 bg-blue-50 overflow-hidden">
       {/* Background split */}
       <div className="absolute inset-0 z-0 flex flex-col lg:flex-row">
-        <div className="h-1/4 lg:h-full lg:w-1/3 bg-white"></div>
-        <div className="h-3/4 lg:h-full lg:w-2/3 bg-blue-50"></div>
+        <div className="h-7/32 lg:h-full lg:w-1/3 bg-white"></div>
+        <div className="h-25/32 lg:h-full lg:w-2/3 bg-blue-50"></div>
       </div>
 
 
