bfb8c5424d130615317d617ea5d73ab8caf13576
Oluwatomisin John Badmus <Tomisinbadmus@gmail.com>
2025-06-19 16:07:19 -0400

    Update navbar.tsx

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -68,12 +68,10 @@ export default function Navbar() {
     e.preventDefault();
     const element = document.querySelector(href);
     if (element) {
-        const navbar = document.querySelector("nav");
-        const navbarHeight = navbar ? navbar.getBoundingClientRect().height : 0;
-        const elementPosition = element.getBoundingClientRect().top;
-        const offsetPosition = elementPosition + window.pageYOffset - navbarHeight;
-
-        window.scrollTo({top: offsetPosition, behavior: "smooth" });
+        window.scrollTo({
+            top: element.getBoundingClientRect().top + window.pageYOffset,
+            behavior: "smooth"
+        });
     }
   }
 
